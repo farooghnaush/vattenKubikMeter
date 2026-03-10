8338a3fb5e0272af85f6a3289e3587bb70c21bad
faroogh <farooghnaushahi@gmail.com>
2026-03-10 16:26:44 +0100

    Py test correction

--- tests.py
+++ tests.py
@@ -5,16 +5,16 @@ class TestWaterCubic(unittest.TestCase):
     """Testfall med olika siffror som representerar terrängens markhöjd."""
     
     def test_data(self):
-        obj = vattenKubikMeter([0,0,0,-1,-1,-1,-2,-3])
+        obj = vattenKubikMeter([0, 0, 0, -1, -1, -1, -2, -3])
         a = obj.beräkna_vatten()
         self.assertEqual(a, 8)
 
-        obj = vattenKubikMeter([-1,2,-1,-2,1,4,-1,2])
+        obj = vattenKubikMeter([-1, 2, -1, -2, 1, 4, -1, 2])
         b = obj.beräkna_vatten()
         self.assertEqual(b, 12)
     
     def test_data2(self):
-        obj = vattenKubikMeter([1,2])
+        obj = vattenKubikMeter([1, 2])
         c = obj.beräkna_vatten()
         self.assertFalse(c, 1)
 
